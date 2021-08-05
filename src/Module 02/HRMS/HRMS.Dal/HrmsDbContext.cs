751f1e0282ac9d72abebab24dd56c9beee64325a
Vineet Yadav <vineetyadav@msn.com>
2021-08-06 04:47:07 +0530

    Module 02 framework added

---
+++ src/Module 02/HRMS/HRMS.Dal/HrmsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using HRMS.Dal.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS.Dal
+{
+    internal class HrmsDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
