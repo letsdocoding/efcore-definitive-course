93a29286adc2df5f93d343227f84df9e928b4bf2
Vineet Yadav <vineetyadav@msn.com>
2021-08-06 02:09:59 +0530

    Module 1 Added - Quickstart

---
+++ src/Module 01/EfCoreCompleteCourse/EfCoreCompleteCourse.TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace EfCoreCompleteCourse.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new UserDbContext();
+            dbContext.Users.Add(new User()
+                {FirstName = "vineet-"+DateTime.Now.ToLongTimeString(), LastName = "yadav", DateOfBirth = new DateTime(2000, 1, 1)});
+            dbContext.SaveChanges();
+
+            var allUsers = dbContext.Users.ToList();
+            foreach (var user in allUsers)
+            {
+                Console.WriteLine($"User Found: Id: {user.Id}, Name:  {user.FirstName}");
+            }
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Module 01/EfCoreCompleteCourse/EfCoreCompleteCourse.TestConsole/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreCompleteCourse.TestConsole
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(LocalDb)\MSSQLLocalDb;Initial Catalog=TestDb;Integrated Security=true;");
+        }
+    }
+}
