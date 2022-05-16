0242ca79593a3d8f074be1ce2d142cd99e0d93f6
Vineet Yadav <vineetyadav@msn.com>
2022-05-16 18:56:20 +0530

    all temporal methods updated

--- src/Additional Concepts/Temporal/HRMS/HRMS.API/Controllers/UsersController.cs
+++ src/Additional Concepts/Temporal/HRMS/HRMS.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -33,8 +34,16 @@ namespace HRMS.API.Controllers
 
             return Content(JsonSerializer.Serialize(user), "application/json");
         }
+
+        //.TemporalAll()
+
+        /// <summary>
+        /// Get All Changes for Users - uses .TemporalAll()
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}/all")]
-        public ActionResult GetUserByIdALlChanges(int id)
+        public ActionResult GetUserByIdAllChanges(int id)
         {
             var user = _dbContext.Users
                 .TemporalAll()
@@ -48,6 +57,104 @@ namespace HRMS.API.Controllers
                 .OrderBy(x=>x.From)
                 .ToList();
             if (!user .Any())
+            {
+                return NotFound();
+            }
+
+            return Content(JsonSerializer.Serialize(user), "application/json");
+        }
+
+        //TemporalAsOf
+        /// <summary>
+        /// Get All Changes for Users - uses .TemporalAsOf()
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="datetime"></param>
+        /// <returns></returns>
+        [HttpGet("{id}/on/{datetime}")]
+        public ActionResult GetUserByIdAllChanges(int id,string datetime)
+        {
+            if (!DateTime.TryParse(datetime, out var pointInTime))
+                return BadRequest();
+            var user = _dbContext.Users
+                .TemporalAsOf(pointInTime)
+                .Where(b => b.UserId == id)
+                .Select(b => new
+                {
+                    User = b,
+                    From = EF.Property<DateTime>(b, "PeriodStart"),
+                    Till = EF.Property<DateTime>(b, "PeriodEnd"),
+                })
+                .OrderBy(x => x.From)
+                .ToList();
+            //SINGLE RECORD ONLY
+            if (!user.Any())
+            {
+                return NotFound();
+            }
+
+            return Content(JsonSerializer.Serialize(user), "application/json");
+        }
+
+        //TemporalBetween
+        /// <summary>
+        /// Get All Changes for Users BETWEEN uses .TemporalBetween()
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="datetimeFrom"></param>
+        /// <param name="datetimeTo"></param>
+        /// <returns></returns>
+        [HttpGet("{id}/from/{datetimeFrom}/till/{datetimeTo}")]
+        public ActionResult GetUserByIdAllChanges(int id, string datetimeFrom, string datetimeTo)
+        {
+            if (!DateTime.TryParse(datetimeFrom, out var pointInTimeFrom) || !DateTime.TryParse(datetimeTo, out var pointInTimeTill))
+                return BadRequest();
+
+            var user = _dbContext.Users
+                .TemporalBetween(pointInTimeFrom, pointInTimeTill)
+                .Where(b => b.UserId == id)
+                .Select(b => new
+                {
+                    User = b,
+                    From = EF.Property<DateTime>(b, "PeriodStart"),
+                    Till = EF.Property<DateTime>(b, "PeriodEnd"),
+                })
+                .OrderBy(x => x.From)
+                .ToList();
+            if (!user.Any())
+            {
+                return NotFound();
+            }
+
+            return Content(JsonSerializer.Serialize(user), "application/json");
+        }
+
+        //TemporalContainedIn
+        /// <summary>
+        /// Get All Changes for Users BETWEEN uses .TemporalContainedIn()
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="datetimeFrom"></param>
+        /// <param name="datetimeTo"></param>
+        /// <returns></returns>
+        [HttpGet("{id}/all/from/{datetimeFrom}/till/{datetimeTo}")]
+        public ActionResult GetAllValidUsers(int id, string datetimeFrom, string datetimeTo)
+        {
+            if (!DateTime.TryParse(datetimeFrom, out var pointInTimeFrom) || !DateTime.TryParse(datetimeTo, out var pointInTimeTill))
+                return BadRequest();
+
+            var user = _dbContext.Users
+                .TemporalContainedIn(pointInTimeFrom, pointInTimeTill)
+                .Where(b => b.UserId == id)
+                .Select(b => new
+                {
+                    User = b,
+                    From = EF.Property<DateTime>(b, "PeriodStart"),
+                    Till = EF.Property<DateTime>(b, "PeriodEnd"),
+                })
+                .OrderBy(x => x.From)
+                .ToList();
+            if (!user.Any())
             {
                 return NotFound();
             }
